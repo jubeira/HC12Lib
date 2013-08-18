e3901e6c30357645a68bd356933466b7dc0e14b7
Juan Ignacio Ubeira <juan.iu@hotmail.com>
2013-08-18 13:32:37 -0300

    Merge with multiple mains and proyects

---
+++ Sources/mainTransmitter.c
@@ -0,0 +1,33 @@
+#include "mc9s12xdp512.h"
+#include "common.h"
+#include "pll.h"
+#include "quick_serial.h"
+#include "quad_rf.h"
+
+void Init (void);
+
+void main (void)
+{
+	Init ();
+
+	qrf_SendJoyMeasurements();
+	qrf_PrintCommInfo();
+
+	while (1)
+		;	
+}
+
+void Init (void)
+{
+	PLL_SPEED(BUS_CLOCK_MHZ);
+
+	// Modules that don't require interrupts to be enabled
+	qs_init(0, MON12X_BR);
+
+	asm cli;
+
+	// Modules that do require interrupts to be enabled
+	qrf_Init();
+
+	return;
+}
